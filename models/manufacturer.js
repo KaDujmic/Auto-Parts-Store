174cd2f028a60921ea2a1bc8dc4a83b51ce946f3
Karlo Dujmic <kd48376@oss.unist.hr>
2023-02-10 15:56:33 +0100

    Create manufacturer model and migration file 00:20

---
+++ models/manufacturer.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class manufacturer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  manufacturer.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'manufacturer',
+    updatedAt: 'updated_at',
+    createdAt: 'created_at'
+  });
+  return manufacturer;
+};
