d9c77d5a897fc23cb6704052c6575e25c1057646
Ivana <gasparovivana@outlook.com>
2023-02-10 14:26:51 +0100

    Create migration file and model for order table 00:30

---
+++ migrations/20230210124530-create-order.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('order', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      user_id: {
+        type: Sequelize.UUID
+      },
+      delivery_address: {
+        type: Sequelize.STRING
+      },
+      delivery_date: {
+        type: Sequelize.DATEONLY
+      },
+      order_date: {
+        type: Sequelize.DATEONLY
+      },
+      order_status: {
+        type: Sequelize.STRING
+      },
+      items: {
+        type: Sequelize.JSONB
+      },
+      final_price: {
+        type: Sequelize.FLOAT
+      },
+      full_price: {
+        type: Sequelize.FLOAT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('order');
+  }
+};
