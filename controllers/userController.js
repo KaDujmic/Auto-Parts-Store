014463ba11514dbb7c633b05a1ddb4f5624ec81f
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-13 13:53:26 +0100

    #0003: Add userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,21 +2,21 @@ const { user } = require('..db/models');
 const crudController = require('./crudController');
 
 exports.getUser = async (res, req) => {
-  crudController.getUser(user, res, req);
+  crudController.findModel(user, res, req);
 };
 
 exports.getAllUsers = async (res, req) => {
-  crudController.getAllUsers(user, res, req);
+  crudController.findAllModel(user, res, req);
 };
 
 exports.updateUser = async (res, req) => {
-  crudController.updateUser(user, res, req);
+  crudController.updateModel(user, res, req);
 };
 
 exports.deleteUser = async (res, req) => {
-  crudController.deleteUser(user, res, req);
+  crudController.deleteModel(user, res, req);
 };
 
 exports.createUser = async (res, req) => {
-  crudController.createUser(user, res, req);
+  crudController.createModel(user, res, req);
 };
