48f47a99ac2645e5daccd69f1fc32c16d4c667bd
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-13 15:28:04 +0100

    #0002: Fix updated_at attribute

--- migrations/06-create-user.js
+++ migrations/06-create-user.js
@@ -37,7 +37,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
-      update_at: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
