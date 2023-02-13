effb327c17e017051fec28a214dad5b885c802ca
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-13 10:33:53 +0100

    #0002: Fix camelcase to snakecase, tablename to singular

--- migrations/20230212134901-create-user.js
+++ migrations/20230212134901-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('user', {
       id: {
         allowNull: false,
         primaryKey: true,
@@ -32,17 +32,17 @@ module.exports = {
       currency: {
         type: Sequelize.STRING
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      update_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
     });
   },
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users', { cascade: true });
+    await queryInterface.dropTable('user', { cascade: true });
   }
 };
