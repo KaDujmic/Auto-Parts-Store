d74d299a000be13bd079120700fffa41986b7f85
Karlo Dujmic <kd48376@oss.unist.hr>
2023-02-14 16:48:50 +0100

    #0007 Create cron-job to check currency and update if needed every hour

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+const { setCurrency } = require('./utils/currencyService');
+const cron = require('node-cron');
 
 const port = process.env.PORT || 8080;
+
+cron.schedule('* * * * *', function () {
+  setCurrency();
+});
 
 app.listen(port, () => {
   console.log(`Server listening on the port  ${port}`);
