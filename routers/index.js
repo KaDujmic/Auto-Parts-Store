24ea21fb39337d479e98aa7780b5da887d711d33
Ivana <gasparovivana@outlook.com>
2023-02-15 13:40:51 +0100

    #0006 Add router for settings

--- routers/index.js
+++ routers/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const itemRouter = require('./itemRouter');
 const orderRouter = require('./orderRouter');
 const userRouter = require('./userRouter');
+const settingsRouter = require('./settingsRouter');
 
 const router = express.Router({ mergeParams: true });
 
 router.use('/item', itemRouter);
 router.use('/order', orderRouter);
 router.use('/user', userRouter);
+router.use('/settings', settingsRouter);
 
 module.exports = router;
