404414f588b3cdb83ff5aa31a34ce6dfd60269ec
Ivana <gasparovivana@outlook.com>
2023-02-16 13:15:27 +0100

    #0006 Export settings helper as a list

--- utils/settingsHelper.js
+++ utils/settingsHelper.js
@@ -1,4 +1,7 @@
-exports.DEFAULT_NOTIFICATION = {
+const NOTIFICATION_LIST = [{
   key: 'order_notification_recurrence',
-  value: '1'
-};
+  value: '1',
+  // eslint-disable-next-line max-len
+  template: 'Dear {full_name},The order you have placed on {order_date} has arrived, and is ready for pickup.Please stop by our shop during our working hours.We are looking forward to seeing you!Auto Parts Store'
+}];
+module.exports = NOTIFICATION_LIST;
