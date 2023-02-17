95e7cecad42b2282dccafe7168a01ea8e1935436
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-17 14:52:18 +0100

    #0009: Change value to JSONB

--- models/settings.js
+++ models/settings.js
@@ -18,8 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       primaryKey: true
     },
-    value: DataTypes.STRING,
-    template: DataTypes.STRING
+    value: DataTypes.JSONB
   }, {
     sequelize,
     freezeTableName: true,
