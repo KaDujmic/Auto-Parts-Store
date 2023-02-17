3fc64bf74b674b550e0ea7c9f2826711555d73ba
Ivana <gasparovivana@outlook.com>
2023-02-17 08:43:48 +0100

    #0013 Add promise to remove quantity from item table

--- utils/orderService.js
+++ utils/orderService.js
@@ -1,11 +1,14 @@
 const { Sequelize, user, item } = require('../models');
 const Op = Sequelize.Op;
-const { NotFoundError } = require('./errors');
+const { NotFoundError, ValidationError } = require('./errors');
 
-function onlyUnique (value, index, array) {
-  return array.indexOf(value) === index;
-}
-function getRandomDate () {
+const checkDuplicateElements = function (array) {
+  const duplicate = array.filter((value, index) => array.indexOf(value) !== index);
+  console.log(typeof (duplicate));
+  if (duplicate.length !== 0) { throw new ValidationError('Can not have duplicate elements'); }
+};
+
+const getRandomDate = function () {
   const today = new Date();
   const future = new Date(today);
   future.setDate(today.getDate() + 3);
@@ -27,25 +30,27 @@ const priceCalculation = function (priceList, itemList) {
 
 exports.checkAllElements = async (model, req, res) => {
   const idList = req.body.itemList.map(el => el.id);
-  const uniqueIdList = idList.filter(onlyUnique);
+  checkDuplicateElements(idList);
   const items = await model.findAll({
     where: {
-      id: { [Op.or]: uniqueIdList }
+      id: { [Op.or]: idList }
     },
     attributes: ['id', 'serialNumber', 'quantity']
   });
-  if (items.length !== uniqueIdList.length) throw new NotFoundError();
+  if (items.length !== idList.length) throw new NotFoundError();
+
   items.forEach(el => {
     const requestItem = req.body.itemList.find(item => item.id === el.id);
     if (el.quantity < requestItem.quantity) {
       req.body.deliveryDate = getRandomDate();
     } else {
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-      const tomorrow = new Date(today);
-      tomorrow.setDate(today.getDate() + 1);
-      req.body.deliveryDate = new Date(tomorrow);
-    };
+      const savePromises = items.map(item => {
+        const requestItem = req.body.itemList.find(requestItem => requestItem.id === item.id);
+        item.quantity -= requestItem.quantity;
+        return item.save();
+      });
+      Promise.all(savePromises);
+    }
   });
 };
 
