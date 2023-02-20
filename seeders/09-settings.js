1713587e3737949525b73d261ed5b2a5ee885c30
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-20 14:34:19 +0100

    #0011: Add all current notifications/recurrence into seeder file

--- seeders/09-settings.js
+++ seeders/09-settings.js
@@ -3,15 +3,54 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('settings', [
+      {
+        key: 'order_confirmation_template',
+        value: {
+          title: 'Auto Parts Store: Your order has been confirmed!',
+          body: `Dear customerName,
+        
+        The order you have placed has been confirmed. We will contact you once your order is ready for pickup. 
+        
+    
+        Thank you for your purchase,
+        Auto Parts Store`
+        }
+      },
+      {
+        key: 'order_arrived_template',
+        value: {
+          title: 'Auto Parts Store: Your order has arrived!',
+          body: `Dear customerName,
+      
+        The order you have placed on orderDate is ready.
+        Please come by our shop during working hours to pick up your order.
+        
+        We are looking forward to seeing you!
+        
+        Auto Parts Store`
+        }
+      },
+      {
+        key: 'order_pickup_template',
+        value: {
+          title: 'Auto Parts Store: Order Reminder',
+          body: `Dear customerName,
+      
+        The order you have placed on orderDate is waiting for pickup. 
+        Please come by our shop during working hours to pick up your order.
+    
+      
+        We are looking forward to seeing you!
+        
+        Auto Parts Store`
+        }
+      },
+      {
+        key: 'order_pickup_recurrence',
+        value: { recurrence: '1' }
+      }
+    ], {}, { value: { type: new Sequelize.JSONB() } });
   },
 
   async down (queryInterface, Sequelize) {}
