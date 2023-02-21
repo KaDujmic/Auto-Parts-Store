57e4038986b15477c6b091fad23f41352267a0c8
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-21 14:01:19 +0100

    #0014: Standardize cache creation function naming

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require('./app');
 const cron = require('node-cron');
 const { setCurrency } = require('./utils/currencyService');
-const { refreshSettings } = require('./utils/settingsService');
+const { setSettings } = require('./utils/settingsService');
 const { sendRecurringEmails } = require('./utils/notificationService');
 
 const port = process.env.PORT || 8080;
 
 cron.schedule('0 * * * *', function () {
   setCurrency();
-  refreshSettings();
+  setSettings();
 });
 
 cron.schedule('0 9 * * *', function () {
@@ -19,5 +19,5 @@ app.listen(port, () => {
   console.log(`Server listening on the port  ${port}`);
   // Set cache keys on startup
   setCurrency();
-  refreshSettings();
+  setSettings();
 });
