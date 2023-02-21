fd35ad0afb41f8692dd3d354a3adc4537d11696d
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-21 14:02:37 +0100

    #0016: Add filter by manufacturer, category, or both

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,22 +1,41 @@
 const { item } = require('../models');
 const crudController = require('./crudController');
 
-exports.getItem = async (res, req) => {
-  await crudController.findModel(item, res, req);
+exports.getItem = async (req, res) => {
+  await crudController.findModel(item, req, res);
 };
 
-exports.getAllItem = async (res, req) => {
-  await crudController.findAllModel(item, res, req);
+exports.getAllItem = async (req, res) => {
+  if (typeof req.query !== 'undefined') {
+    const query = { where: [] };
+    query.where = getWhereOption(req.query);
+
+    const foundItems = await item.findAll(query);
+    res.status(200).json(foundItems);
+  } else await crudController.findAllModel(item, req, res);
 };
 
-exports.updateItem = async (res, req) => {
-  await crudController.updateModel(item, res, req);
+exports.updateItem = async (req, res) => {
+  await crudController.updateModel(item, req, res);
 };
 
-exports.deleteItem = async (res, req) => {
+exports.deleteItem = async (req, res) => {
   await crudController.deleteModel(item, res, req);
 };
 
-exports.createItem = async (res, req) => {
-  await crudController.createModel(item, res, req);
+exports.createItem = async (req, res) => {
+  await crudController.createModel(item, req, res);
 };
+
+function getWhereOption (queryParams) {
+  const whereOption = [];
+
+  if (queryParams.category) {
+    whereOption.push({ category_id: queryParams.category });
+  }
+  if (queryParams.manufacturer) {
+    whereOption.push({ manufacturer_id: queryParams.manufacturer });
+  }
+
+  return whereOption;
+}
