b58d8bf2869c13cce25227e1537ef228e3f5006f
Karlo Dujmic <kd48376@oss.unist.hr>
2023-02-22 14:52:47 +0100

    #0028 Update spelling

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 const { order, item, order_item } = require('../models');
 const crudController = require('../controllers/crudController');
-const { checkAllElements, setOrderPrice, retriveItemOnOrder } = require('../utils/orderService');
+const { checkAllElements, setOrderPrice, retrieveItemOnOrder } = require('../utils/orderService');
 const { orderConfirmEmail, orderArrivedEmail } = require('../utils/notificationService');
 exports.getAllOrders = async (req, res) => {
   await crudController.findAllModel(order, req, res);
@@ -45,7 +45,7 @@ exports.deleteOrder = async (req, res) => {
 
 exports.confirmOrder = async (req, res) => {
   const pendingOrder = await order.findByPk(req.params.id);
-  await retriveItemOnOrder(pendingOrder.dataValues, req, res);
+  await retrieveItemOnOrder(pendingOrder.dataValues, req, res);
   pendingOrder.orderStatus = 'pending_delivery';
   pendingOrder.save();
   orderConfirmEmail(pendingOrder.userId);
