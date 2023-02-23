53c0baed874530e248beb544398a4bfe34a213fc
Karlo Dujmic <kd48376@oss.unist.hr>
2023-02-23 14:50:47 +0100

    #0021 Create jest config file

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+require('dotenv').config({
+  path: '.env.test'
+});
+
+const config = {
+  // reporters: [
+  //   'default',
+  //   ['./node_modules/jest-html-reporter', { pageTitle: 'Test Report' }]
+  // ],
+  collectCoverage: true
+};
+
+module.exports = config;
