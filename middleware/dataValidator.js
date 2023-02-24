4f230873649cafe99da020a300974db94ea54b61
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-02-24 11:15:50 +0100

    #0031: Add comments

--- middleware/dataValidator.js
+++ middleware/dataValidator.js
@@ -5,10 +5,10 @@ exports.bodyValidator = async (req, res, next) => {
   let schema = null;
   const schemaName = req.baseUrl.slice(1) + 'Schema';
 
+  // Accepted body properties depend on whether it is a PUT or POST Request
   schema = joiSchemaList[schemaName].tailor(req.method.toLowerCase());
 
   const { error } = schema.validate(req.body);
-  // TODO: Expect possible multiple error messages that should be bundled into one readable list
   if (error) next(new ValidationError(error.details[0].message));
 
   next();
