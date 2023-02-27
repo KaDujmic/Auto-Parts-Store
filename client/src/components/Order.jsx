51b59e246eeadb8b10e4e745e9b03a9c7ecd1eda
Karlo Dujmic <kd48376@oss.unist.hr>
2023-02-27 16:03:48 +0100

    #0037 Create basic layout for order component

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -1,58 +1,80 @@
 import * as React from 'react';
+import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Grid from '@material-ui/core/Grid';
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+import Grid from '@mui/material/Grid';
 
 const card = (
-  <React.Fragment>
-     <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: '100vh' }}
+  <>
+    <Paper
+      sx={{
+        p: 2,
+        margin: '5%',
+        padding: '4%',
+        maxWidth: 500,
+        backgroundColor: '#d3d3d3',
+        flexGrow: 1,
+      }}
     >
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Grid>
-  </React.Fragment>
+      <Grid
+          container
+          spacing={2}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          sx={8}
+      >
+        <Grid item xs={12} container>
+          <Grid item xs={6} alignContent="center">
+            <Typography variant='h5'>Customer:</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant='h5'>John Doe</Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} container>
+          <Grid item xs={6} alignContent="center">
+            <Typography variant='h5'>Order Number:</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant='h5'>#0002314</Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} container>
+          <Grid item xs={6} alignContent="center">
+            <Typography variant='h5'>Ordered Date:</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant='h5'>Nov 10th, 2023</Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} container>
+          <Grid item xs={6} alignContent="center">
+            <Typography variant='h5'>Status:</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant='h5'>Ready For Pickup</Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} container>
+          <Grid item xs={6} alignContent="center">
+            <Typography variant='h5'>Price:</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant='h5'>19942.00 $</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  </>
 );
 
 export default function OutlinedCard() {
   return (
-    <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined">{card}</Card>
-    </Box>
+      card
   );
 }
