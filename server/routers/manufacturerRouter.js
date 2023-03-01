e0b05573966af45dd0b9e0f97dcf6027b3ecdca7
Ivana <gasparovivana@outlook.com>
2023-03-01 10:46:53 +0100

    #0043 Add filter by category and manufacturer

--- server/routers/manufacturerRouter.js
+++ server/routers/manufacturerRouter.js
@@ -5,16 +5,16 @@ const router = express.Router({ mergeParams: true });
 const { bodyValidator } = require('../middleware/dataValidator');
 const { isLoggedIn, restrictTo } = require('../controllers/authController');
 
+router.get('/', callbackErrorHandler(getAllManufacturer));
+router.get('/:id', callbackErrorHandler(getManufacturer));
 router.use(callbackErrorHandler(isLoggedIn));
 
 router
   .route('/')
-  .get(restrictTo('Salesperson', 'Customer'), callbackErrorHandler(getAllManufacturer))
   .post(restrictTo('Salesperson'), bodyValidator, callbackErrorHandler(createManufacturer));
 
 router
   .route('/:id')
-  .get(restrictTo('Salesperson', 'Customer'), callbackErrorHandler(getManufacturer))
   .put(restrictTo('Salesperson'), bodyValidator, callbackErrorHandler(updateManufacturer))
   .delete(restrictTo('Salesperson'), callbackErrorHandler(deleteManufacturer));
 
