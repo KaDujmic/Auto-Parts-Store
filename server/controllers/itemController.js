33f3840717ee48d1b8bf76fcd3ebdd2a0759a3ae
Goran Stajduhar <goran.stajduhar@agilathon.com>
2023-03-03 12:07:48 +0100

    Remove console.log

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -12,7 +12,6 @@ exports.getManyItem = async (req, res) => {
 
   if (Object.keys(query).length !== 0 && req.query.page !== 'count') {
     // Find filtered items
-    console.log(query);
     await crudController.findManyModel(item, query, req, res);
   } else {
     // Find complete list of Items
