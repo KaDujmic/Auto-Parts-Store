66e0775f6686e6c2f22cef00a518bde1fa7fcb87
Karlo Dujmic <kd48376@oss.unist.hr>
2023-03-07 13:54:33 +0100

    #0054 Create helper file for constants

---
+++ server/utils/helper.js
@@ -0,0 +1,11 @@
+exports.orderStatuses = {
+  pending_confirmation: 'pending_confirmation',
+  pending_delivery: 'pending_delivery',
+  ready_for_pickup: 'ready_for_pickup',
+  completed: 'completed'
+};
+
+exports.itemStatuses = {
+  pending: 'pending',
+  delivered: 'delivered'
+};
