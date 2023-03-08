7ff344e130bddf7db35204af80413563a825afea
Ivana <gasparovivana@outlook.com>
2023-03-08 14:23:14 +0100

    Move min and max into values

--- server/services/settingsService.js
+++ server/services/settingsService.js
@@ -1,12 +1,17 @@
 const { ValidationError } = require('../validators/errors.js');
 const cache = require('../utils/cache');
 const { settings } = require('../db/models');
+const MIN_VALUE = 1;
+const MAX_VALUE = 10;
+const KEY_STRING = 'recurrence';
 
 // Create restraint for recurrence
 exports.verifySettings = async (req) => {
   const { key, value } = req.body;
-  if (key.includes('recurrence')) {
-    if (value < 1 || value > 10) { throw new ValidationError('ERROR: Supported recurrence is between 1, and 10 days.'); }
+  if (key.includes(KEY_STRING)) {
+    if (value < MIN_VALUE || value > MAX_VALUE) {
+      throw new ValidationError(`ERROR: Supported recurrence is between ${MIN_VALUE}, and ${MAX_VALUE} days.`);
+    }
   }
 };
 
