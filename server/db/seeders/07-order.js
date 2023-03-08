df05f20661e4e8015e619b03cd172adfa845b9fe
Ivana <gasparovivana@outlook.com>
2023-03-08 14:22:04 +0100

    Add order date

--- server/db/seeders/07-order.js
+++ server/db/seeders/07-order.js
@@ -8,6 +8,7 @@ module.exports = {
         id: 'a1e679a5-f5a6-4070-8f3d-91ea156bcc1b',
         user_id: '20c1297e-58f6-4587-842b-231ff6583086',
         order_status: 'pending_confirmation',
+        order_date: new Date().toISOString().split('T')[0],
         item_list: [
           { id: 'f0d0ea4d-0e40-4987-866e-154da59552c5', quantity: 2 },
           { id: '0d7c9848-2a69-407e-bfcf-9b1079fc1121', quantity: 51 }
@@ -23,6 +24,7 @@ module.exports = {
         id: '5de49635-dbd9-4ecd-9705-53731f42033b',
         user_id: '805a10d7-1735-4a6c-a4cd-0be767aaeca1',
         order_status: 'pending_confirmation',
+        order_date: new Date().toISOString().split('T')[0],
         item_list: [
           { id: 'f0d0ea4d-0e40-4987-866e-154da59552c5', quantity: 5 },
           { id: '0d7c9848-2a69-407e-bfcf-9b1079fc1121', quantity: 50 }
@@ -38,6 +40,7 @@ module.exports = {
         id: 'fdc0d18f-55f9-48be-b42e-9979da9d69f2',
         user_id: '805a10d7-1735-4a6c-a4cd-0be767aaeca1',
         order_status: 'ready_for_pickup',
+        order_date: new Date().toISOString().split('T')[0],
         item_list: [
           { id: 'f0d0ea4d-0e40-4987-866e-154da59552c5', quantity: 5 },
           { id: '0d7c9848-2a69-407e-bfcf-9b1079fc1121', quantity: 50 }
@@ -53,6 +56,7 @@ module.exports = {
         id: '501f51c5-e288-4771-a415-d26c9e4b63b7',
         user_id: '805a10d7-1735-4a6c-a4cd-0be767aaeca1',
         order_status: 'pending_confirmation',
+        order_date: new Date().toISOString().split('T')[0],
         item_list: [
           { id: 'f0d0ea4d-0e40-4987-866e-154da59552c5', quantity: 5 },
           { id: '0d7c9848-2a69-407e-bfcf-9b1079fc1121', quantity: 50 }
@@ -68,6 +72,7 @@ module.exports = {
         id: '331bdd11-040f-467a-84f5-32e78606f5e9',
         user_id: '805a10d7-1735-4a6c-a4cd-0be767aaeca1',
         order_status: 'pending_delivery',
+        order_date: new Date().toISOString().split('T')[0],
         item_list: [
           { id: 'f0d0ea4d-0e40-4987-866e-154da59552c5', quantity: 5 },
           { id: '0d7c9848-2a69-407e-bfcf-9b1079fc1121', quantity: 50 }
@@ -82,6 +87,7 @@ module.exports = {
         id: '94aa7d75-08fa-495a-84ef-b077edbe5e3a',
         user_id: '20c1297e-58f6-4587-842b-231ff6583086',
         order_status: 'pending_confirmation',
+        order_date: new Date().toISOString().split('T')[0],
         item_list: [
           { id: 'f0d0ea4d-0e40-4987-866e-154da59552c5', quantity: 5 },
           { id: '0d7c9848-2a69-407e-bfcf-9b1079fc1121', quantity: 50 }
